3c127e7ca617d9adc972fc578043bf10ea788795
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-02-19 10:06:06 -0500

    Push to commit completed function in JS, setTimer().

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,8 +1,11 @@
+//Variables
 var timerEl = document.querySelector('.timer-element');
 var quizEl = document.querySelector('.quiz-body');
 var startBtn = document.getElementById('start-quiz');
+var highscoreEl = document.getElementById('highscores');
+var secondsLeft = 75;
 var questions = [
-    {   
+    {
         question: 'What is Javascript?',
         choices: ['JavaScript is a scripting language used to make the website interactive', 'JavaScript is an assembly language used to make the website interactive', 'JavaScript is a compiled language used to make the website interactive', 'None of the mentioned'],
         answer: [0]
@@ -16,13 +19,13 @@ var questions = [
 
     {
         question: 'Arrays in JavaScript are defined by which of the following statements?',
-        choices: ['It is an ordered list of values', 'It is an ordered list of objects', 'It is an ordered list of string' ,'It is an ordered list of functions'],
+        choices: ['It is an ordered list of values', 'It is an ordered list of objects', 'It is an ordered list of string', 'It is an ordered list of functions'],
         answer: [0]
     },
 
     {
         question: 'What element is Javascript enclosed in when entered into HTML?',
-        choices: ['<javascript>', '<js>', '<scripting>' ,'<script>'],
+        choices: ['<javascript>', '<js>', '<scripting>', '<script>'],
         answer: [3]
     },
 
@@ -39,30 +42,57 @@ var questions = [
     },
 
     {
-        question: 'Arrays in JavaScript are defined by which of the following statements?',
-        choices: ['It is an ordered list of values', 'It is an ordered list of objects', 'It is an ordered list of string' ,'It is an ordered list of functions'],
-        answer: [0]
+        question: 'How do you create a function in JavaScript?',
+        choices: ['function:myFunction()', 'function = myFunction()', 'function myFunction()'],
+        answer: [2]
     },
 
     {
-        question: 'Arrays in JavaScript are defined by which of the following statements?',
-        choices: ['It is an ordered list of values', 'It is an ordered list of objects', 'It is an ordered list of string' ,'It is an ordered list of functions'],
-        answer: [0]
+        question: 'How do you write "Hello World" in an alert box?',
+        choices: ['msgBox("Hello World");', 'alertBox("Hello World");', 'alert("Hello World");', 'msg("Hello World");'],
+        answer: [2]
     },
 
     {
-        question: 'Arrays in JavaScript are defined by which of the following statements?',
-        choices: ['It is an ordered list of values', 'It is an ordered list of objects', 'It is an ordered list of string' ,'It is an ordered list of functions'],
-        answer: [0]
+        question: 'How to write an IF statement in JavaScript?',
+        choices: ['if i == 5 then', 'if i = 5 then', 'if i = 5', 'if (i == 5)'],
+        answer: [3]
     },
 
     {
-        question: 'Arrays in JavaScript are defined by which of the following statements?',
-        choices: ['It is an ordered list of values', 'It is an ordered list of objects', 'It is an ordered list of string' ,'It is an ordered list of functions'],
-        answer: [0]
+        question: 'How does a FOR loop start?',
+        choices: ['for (i = 0; i <= 5)', 'for i = 1 to 5', 'for (i = 0; i <= 5; i++)', 'for (i <= 5; i++)'],
+        answer: [2]
     }
-]
+];
 
+//Functions
 function startGame() {
 
 }
+
+function questionAndAnswer() {
+
+}
+
+function setTimer() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerEl.textContent = 'Time: ' + secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            saveHighscore();
+        }
+    }, 1000);
+}
+
+function highscore() {
+
+}
+
+function saveHighscore() {
+
+}
+
+//Initiators
